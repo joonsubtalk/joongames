d1b3618fb3da5bd73d2f49b20b3671665b1cbd64
joon <joonsubtalk@gmail.com>
2018-06-30 19:09:50 -0700

    fixes

---
+++ src/Components/Characters/Character.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Character extends Component {
+  render() {
+    return (
+      <div className="c-character">
+        <div className="c-character__head"></div>
+      </div>
+    )
+  }
+}
+
+export default Character;
--- src/Components/Levels/Lvl001/Lvl001.jsx
+++ src/Components/Levels/Lvl001/Lvl001.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import Header from '../../Header/Header';
+import Character from '../../Characters/Character';
 
 class Level001 extends Component {
 
     state = {
         level: 0,
-        title: 'Once upon a time...'
+        title: 'Once upon a time...',
+        fps :  50
+    }
+
+    componentDidMount() {
+
+    }
+
+    update = () => {
+        // updates
+    }
+
+    draw = () => {
+        // actions
+    }
+
+    run = () => {
+        this.update();
+        this.draw();
+    }
+
+    startTimer = () => {
+
+        // intervalID = setInterval(this.run, 1000/this.state.fps);
+
+    }
+
+    endTimer = () => {
+
+        // clearInterval(intervalID);
+
+    }
+
+    btnHandler = (e) => {
+
+        console.log(e.target.innerText)
     }
 
     render() {
@@ -19,7 +55,22 @@ class Level001 extends Component {
 
                     <div className="o-stage__camera">
 
+                        <Character />
+
+                        <div className="o-stage__buttons">
+                            <button onClick={this.btnHandler} className="o-stage__button">1</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">2</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">3</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">4</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">5</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">6</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">7</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">8</button>
+                            <button onClick={this.btnHandler} className="o-stage__button">9</button>
+                        </div>
+
                     </div>
+
                 </div>
 
             </div>
